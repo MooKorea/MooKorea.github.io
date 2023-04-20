b0d3788462bc4638b46f0fc742fbfe4e1bf35883
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-04-20 12:22:15 -0500

    navbar & responsive css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Documentation from './assets/pages/documentation'
+import Navbar from './assets/components/Navbar'
 import './assets/styles/index.scss'
 
 export default function App() {
   return (
-    <Documentation />
+    <>
+      <Navbar />
+      <Documentation />
+    </> 
   )
 }
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className='container'>
+        <ul>
+            <li>Home</li>
+            <li>Download</li>
+            <li>Features</li>
+            <li>Tutorials</li>
+            <li>Documentation</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
